c04ee611a5cdbd37e85300cd39725ab7330e1f16
jakefeasel <jfeasel@gmail.com>
2012-11-11 16:29:00 -0800

    New tests for date parsing Fixes for Chrome to reject things that really shouldn't be treated as dates

--- ddl_builder/qunit/main.js
+++ ddl_builder/qunit/main.js
@@ -5,12 +5,14 @@ define([
 	"./columnTypes",
 	"./guessValueSeparators",
 	"./headerNames",
-	"./recordCount"
+	"./recordCount", 
+	"DDLBuilder/ddl_builder"
 	], 
 
 	function ($,QUnit,fixtureContent,
 			columnTypes,guessValueSeparators,
-			headerNames,recordCount) {
+			headerNames,recordCount,
+			DDLBuilder) {
 		
 		$("#qunit-fixture").append(fixtureContent);
 		
@@ -24,5 +26,24 @@ define([
 			});
 		});
 		
+		QUnit.test("Various date format parsing attempts", function () {
+			var content = $("#qunit-fixture #dateParse").html(),
+				ddl_builder = new DDLBuilder({ddlTemplate: "[{{#each_with_index data}}{{#if index}},{{/if}}[{{#each_with_index r}}{{#if index}}, {{/if}}\"{{formatted_field ../..}}\"{{/each_with_index}}]{{/each_with_index}}]"}),
+				result = ddl_builder.parse(content),
+				parsedResult=null,i=0;
+
+			try {
+				parsedResult = $.parseJSON(result); 
+			} catch (err){}
+			
+			if (parsedResult) {
+				for (i in parsedResult) {
+					QUnit.equal(parsedResult[i][0], parsedResult[i][1], "Input date matches output date");
+				}
+			}
+			else
+				QUnit.ok(false, "Reading date data failed: Unable to parse result to JSON array ("+ result +")");
+			
+		});
 	}
-)
+);
